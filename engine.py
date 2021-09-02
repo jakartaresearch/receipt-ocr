4f932a8b450379467ddeb6280187d2348bd5399f
rubentea16 <rstefanus16@gmail.com>
2021-09-02 22:51:12 +0700

    feat: get image from bb

--- engine.py
+++ engine.py
@@ -1,12 +1,43 @@
+import cv2
 from model import ReceiptOCR_Model
 from text_detector.infer import inference as infer_detector
+
 
 class ReceiptOCR_Engine():
     def __init__(self, receipt_ocr_model: ReceiptOCR_Model):
         if not isinstance(receipt_ocr_model, ReceiptOCR_Model):
             raise TypeError
         self._model: ReceiptOCR_Model = receipt_ocr_model
-    
-    def infer(self, image_path):
-        output = infer_detector(self._model.cfg_detector, self._model.detector, image_path)
-        return output
+
+    def inference_detector(self):
+        output = infer_detector(self._model.cfg_detector,
+                                self._model.detector, self._img_pth)
+        self._out_detector = output
+
+    def predict(self, image_path):
+        self._img_pth = image_path
+        self._img = cv2.imread(image_path)
+
+        self.inference_detector()
+        self.get_img_from_bb()
+
+    def get_img_from_bb(self):
+        imgs = []
+        for bb in self._out_detector:
+            imgs.append(self.crop_img(self._img, bb))
+        self._imgs = imgs
+
+    def crop_img(self, img, bb):
+        x1, y1 = bb[0]
+        x2, y2 = bb[1]
+        x3, y3 = bb[2]
+        x4, y4 = bb[3]
+
+        top_left_x = min([x1, x2, x3, x4])
+        top_left_y = min([y1, y2, y3, y4])
+        bot_right_x = max([x1, x2, x3, x4])
+        bot_right_y = max([y1, y2, y3, y4])
+
+        cropped_image = img[int(top_left_y):int(
+            bot_right_y)+1, int(top_left_x):int(bot_right_x)+1]
+        return cropped_image
--- model.py
+++ model.py
@@ -1,11 +1,24 @@
 from text_detector.load_model import load_craft
+from text_recognizer.load_model import load_star
+
 
 class ReceiptOCR_Model():
-    def __init__(self, detector_cfg):
+    def __init__(self, detector_cfg, recognizer_cfg):
         self._cfg_detector, self._detector = load_craft(detector_cfg)
+        self._cfg_recognizer, self._recognizer = load_star(recognizer_cfg)
 
     @property
-    def cfg_detector(self): return self._cfg_detector
+    def cfg_detector(self):
+        return self._cfg_detector
 
     @property
-    def detector(self): return self._detector
+    def detector(self):
+        return self._detector
+
+    @property
+    def cfg_recognizer(self):
+        return self._cfg_recognizer
+
+    @property
+    def recognizer(self):
+        return self._recognizer
